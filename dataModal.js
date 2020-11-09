62cdbcbc1e09f213e9595ce3b3e2e1e83cc0802a
Nicosen <SUCCESSW2M@163.com>
2020-11-09 14:59:51 +0800

    排序和 查找

--- dataModal.js
+++ dataModal.js
@@ -1265,6 +1265,109 @@ function Graph(){
 // 两种算法可以对图进行遍历 ： 广度优先（Breadth-First Search,BFS） 和 深度优先(Depth-First Search  DFS)
 // 图的遍历可以用来寻找 特定的顶点或 两个顶点之间的路径，检查图是否连通，检查是否含有环等
 
+// 排序和搜索算法
+function ArrayList(){
+	let array = [];
+	this.insert = function(ele){
+		array.push(ele);
+	}
+	this.string = function(){
+		return array.join();
+	}
+	var swap = function(index1, index2) {
+	    var aux = array[index1];
+	    array[index1] = array[index2];
+	    array[index2] = aux;
+	};
+	// 冒泡排序
+	this.bubbleSort = function() {
+	    var length = array.length;
+	    for (var i = 0; i < length; i++) {
+	        for (var j = 0; j < length - 1; j++) {
+	            if (array[j] > array[j + 1]) {
+	                [array[j], array[j + 1], ] = [array[j + 1], array[j]];
+	            }
+	        }
+	    }
+	}	
+	// 改进后的冒泡排序
+	this.modifiedBubbleSort = function() {
+	    var length = array.length;
+	    for (var i = 0; i < length; i++) {
+	        for (var j = i; j < length - 1 - i; j++) { //{1}
+	            if (array[j] > array[j + 1]) {
+	                swap(j, j + 1);
+	                // 或者  
+	                // [array[j],array[j+1],] = [array[j+1],array[j]];
+	            }
+	        }
+	    }
+	};
+	// 选择排序
+	// 原理=>>>选择排序算法是一种原址比较排序算法。选择排序大致的思路是找到数据结构中的最小值并
+	// 将其放置在第一位，接着找到第二小的值并将其放在第二位
+	this.selectionSort = function(){
+		var length = array.length,indexMin;
+		for(var i = 0;i< length;i++){
+			indexMin = i;
+			for(var j = 0;j<length;j++){
+				if(array[indexMin] > array[j]){
+					indexMin = j;
+				}
+			}
+			if(i !== indexMin){
+				swap(i, indexMin);
+			}
+		}
+	}
+	// 插入排序 插入排序每次排一个数组项，以此方式构建最后的排序数组
+	this.insertSort = function (){
+		var length = array.length,j,temp;
+		for(var i=1;i<length;i++){
+			j = i;
+			temp = array[i];
+			while(j>0 && array[j-1] > temp){
+				array[j] = array[j-1];
+				j--;
+			}
+			array[j] = temp;
+		}
+	}
+	// 归并排序算法  归并排序是一种分治算法。其思想是将原始数组切分成较小的数组，直到每个小数组只有一
+	// 个位置，接着将小数组归并成较大的数组，直到最后只有一个排序完毕的大数组 复杂度为O(nlogn)。
+	this.mergeSort = function(){
+		array = mergeSortRec(array);
+	}
+	var mergeSortRec = function (array){
+		var length = array.length;
+		if(length ==1){
+			return array;
+		}
+		var mid = Math.floor(length/2),
+		left = array.slice(0,mid),
+		right = array.slice(mid,length);
+		return merge(mergeSortRec(left),mergeSortRec(right));
+	}
+	var merge = function(left,right){
+		var result = [],
+		il = 0,
+		ir = 0;
+		while(il < left.length && ir < right.length){
+			if (left[il] < right[ir]) {
+			    result.push(left[il++]); 
+			} else {
+			    result.push(right[ir++]); 
+			}
+		}
+		while (il<left.length) {
+			 result.push(left[il++]); 
+		}
+		while (ir < right.length) {
+			result.push(right[ir++]); 
+		}
+		return result;
+	}
+}
 
 
 
