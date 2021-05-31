667c7d2666c3d47b50a4f06ce06c3f68f4e84c29
Nicosen <SUCCESSW2M@163.com>
2021-05-31 17:13:52 +0800

    字符串

--- demo.js
+++ demo.js
@@ -125,3 +125,87 @@ function isPalindrome(str) {
     }
     return true
 }
+
+
+// 真题描述：给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+
+// 示例 1:
+
+// 输入: "aba"
+// 输出: True
+// 示例 2:
+// 输入: "abca"
+// 输出: True
+// 解释: 你可以删除c字符。
+// 注意: 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
+
+var validPalindrome = function(s){
+	const len = s.length;
+	let i=0,j = len-1;
+	whild(i<j && s[i] === s[j]){
+		i++;
+		j--;
+	}
+	if(isPalindrome(i,j-1)){
+		return true
+	}
+	if(isPalindrome(i+1,j)){
+		return true;
+	}
+	function isPalindrome(st,ed){
+		while(st<ed){
+			if(s[st] !== s[ed]){
+				return false;
+			}
+			st++;
+			ed--;
+		}
+		return true;
+	}
+	return false;
+}
+
+
+// 真题描述： 设计一个支持以下两种操作的数据结构：
+
+// void addWord(word)
+// bool search(word)
+// search(word) 可以搜索文字或正则表达式字符串，字符串只包含字母 . 或 a-z 。
+// . 可以表示任何一个字母。
+
+// 示例:
+
+// addWord("bad")
+// addWord("dad")
+// addWord("mad")
+// search("pad") -> false
+// search("bad") -> true
+// search(".ad") -> true
+// search("b..") -> true
+// 说明:
+// 你可以假设所有单词都是由小写字母 a-z 组成的
+
+const WordDictionary = function(){
+	this.words = {};
+}
+WordDictionary.prototype.addWord = function(word){
+	if(this.words[word.length]){
+		this.words[word.length].push(word);
+	}else{
+		this.words[word.length] = [word];
+	}
+};
+
+wordDictionary.prototype.search = function(word){
+	if(!this.words[word.length]){
+		return false;
+	}
+	const le = word.length;
+	if(!word.include('.')){
+		return this.words[len].include(word);
+	}
+	const reg = new RegExp(word);
+	return this.words[len].some((item)=>{
+		return reg.test(item);
+	});
+}
