313247ffb9e1cfbaf2891856f711b8efdf60250d
Nicosen <SUCCESSW2M@163.com>
2021-06-29 16:00:35 +0800

    dfs的应用

--- demo.js
+++ demo.js
@@ -6,16 +6,16 @@
 
 // 给定 nums = [2, 7, 11, 15], target = 9
 // 因为 nums[0] + nums[1] = 2 + 7 = 9 所以返回 [0, 1]
-var twoSum =function(ary,target){
-	let result = {};
-	let current ;
-	ary.map((value,index)=>{
-		if(result[target-value] !=undefined){
-			return current = [result[target-value],index];
-		}
-		result[value] = index;
-	});
-	return current;
+var twoSum = function(ary, target) {
+    let result = {};
+    let current;
+    ary.map((value, index) => {
+        if (result[target - value] != undefined) {
+            return current = [result[target - value], index];
+        }
+        result[value] = index;
+    });
+    return current;
 }
 // var num = [2,7,11,15] 
 // twoSum(num,9)
@@ -31,23 +31,25 @@ var twoSum =function(ary,target){
 // nums1 = [1,2,3,0,0,0], m = 3
 // nums2 = [2,5,6], n = 3
 // 输出: [1,2,2,3,5,6]
-var merge = function(num1,m,num2,n){
-	let i = m-1,j = n-1,k= m+n-1;
-	while (i>=0 && j>=0){
-		if(num1[i] >= num2[j]){
-			num1[k] = num1[i];
-			i--;
-		}else {
-			num1[k] = num2[j];
-			j--;
-		}
-		k--;
-	}
-	while (j>=0){
-		num1[k] = num2[j];
-		k--;
-		j--;
-	}
+var merge = function(num1, m, num2, n) {
+    let i = m - 1,
+        j = n - 1,
+        k = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        if (num1[i] >= num2[j]) {
+            num1[k] = num1[i];
+            i--;
+        } else {
+            num1[k] = num2[j];
+            j--;
+        }
+        k--;
+    }
+    while (j >= 0) {
+        num1[k] = num2[j];
+        k--;
+        j--;
+    }
 }
 
 // 3 三数求和
@@ -60,51 +62,51 @@ var merge = function(num1,m,num2,n){
 
 // 给定数组 nums = [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
 
-var threeSum = (nums)=>{
-	let res = [];
-	// 数组排序
-	nums = nums.sort((a,b)=> (a-b) );
-	const len = nums.length;
+var threeSum = (nums) => {
+    let res = [];
+    // 数组排序
+    nums = nums.sort((a, b) => (a - b));
+    const len = nums.length;
 
-	for(var i=0;i<len-2;i++){
-		// 左指针
-		let j = i+1;
-		// 右指针
-		let k = len-1;
-		// 遇到重复数字  跳过
-		if(i>0 && nums[i] === nums[i-1]){
-			continue;
-		}
-		while (j<k) {
-			if(nums[i] + nums[j] + num[k] <0){
-				j++;
-				// 处理左指针元素重复的情况
-				while(j<k && nums[j] ===nums[j-1]){
-					j++;
-				}
-			}else if(nums[i] + nums[j] + num[k] > 0){
-				k--;
-				// 处理指针元素重复的情况
-				while(j<k && nums[k] ===nums[k+1]){
-					k++;
-				}
-			}else{
-				res.push([nums[i],nums[j],nums[k]]);
-				j++;
-				k--;
-				// 处理左指针元素重复的情况
-				while(j<k && nums[j] ===nums[j-1]){
-					j++;
-				}
-				// 处理指针元素重复的情况
-				while(j<k && nums[k] ===nums[k+1]){
-					k++;
-				}
-			}
-		}
-	}
-	return res;
-} 
+    for (var i = 0; i < len - 2; i++) {
+        // 左指针
+        let j = i + 1;
+        // 右指针
+        let k = len - 1;
+        // 遇到重复数字  跳过
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        while (j < k) {
+            if (nums[i] + nums[j] + num[k] < 0) {
+                j++;
+                // 处理左指针元素重复的情况
+                while (j < k && nums[j] === nums[j - 1]) {
+                    j++;
+                }
+            } else if (nums[i] + nums[j] + num[k] > 0) {
+                k--;
+                // 处理指针元素重复的情况
+                while (j < k && nums[k] === nums[k + 1]) {
+                    k++;
+                }
+            } else {
+                res.push([nums[i], nums[j], nums[k]]);
+                j++;
+                k--;
+                // 处理左指针元素重复的情况
+                while (j < k && nums[j] === nums[j - 1]) {
+                    j++;
+                }
+                // 处理指针元素重复的情况
+                while (j < k && nums[k] === nums[k + 1]) {
+                    k++;
+                }
+            }
+        }
+    }
+    return res;
+}
 
 
 // 判断字符串是不是回文字符
@@ -114,12 +116,13 @@ function isPalindrome(str) {
     // 判断反转前后是否相等
     return reversedStr === str
 }
+
 function isPalindrome(str) {
     // 缓存字符串的长度
     const len = str.length
     // 遍历前半部分，判断和后半部分是否对称
-    for(let i=0;i<len/2;i++) {
-        if(str[i]!==str[len-i-1]) {
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - i - 1]) {
             return false
         }
     }
@@ -139,30 +142,32 @@ function isPalindrome(str) {
 // 解释: 你可以删除c字符。
 // 注意: 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
 
-var validPalindrome = function(s){
-	const len = s.length;
-	let i=0,j = len-1;
-	whild(i<j && s[i] === s[j]){
-		i++;
-		j--;
-	}
-	if(isPalindrome(i,j-1)){
-		return true
-	}
-	if(isPalindrome(i+1,j)){
-		return true;
-	}
-	function isPalindrome(st,ed){
-		while(st<ed){
-			if(s[st] !== s[ed]){
-				return false;
-			}
-			st++;
-			ed--;
-		}
-		return true;
-	}
-	return false;
+var validPalindrome = function(s) {
+    const len = s.length;
+    let i = 0,
+        j = len - 1;
+    while(i < j && s[i] === s[j]) {
+        i++;
+        j--;
+    }
+    if (isPalindrome(i, j - 1)) {
+        return true
+    }
+    if (isPalindrome(i + 1, j)) {
+        return true;
+    }
+
+    function isPalindrome(st, ed) {
+        while (st < ed) {
+            if (s[st] !== s[ed]) {
+                return false;
+            }
+            st++;
+            ed--;
+        }
+        return true;
+    }
+    return false;
 }
 
 
@@ -185,29 +190,29 @@ var validPalindrome = function(s){
 // 说明:
 // 你可以假设所有单词都是由小写字母 a-z 组成的
 
-const WordDictionary = function(){
-	this.words = {};
+const WordDictionary = function() {
+    this.words = {};
 }
-WordDictionary.prototype.addWord = function(word){
-	if(this.words[word.length]){
-		this.words[word.length].push(word);
-	}else{
-		this.words[word.length] = [word];
-	}
+WordDictionary.prototype.addWord = function(word) {
+    if (this.words[word.length]) {
+        this.words[word.length].push(word);
+    } else {
+        this.words[word.length] = [word];
+    }
 };
 
-wordDictionary.prototype.search = function(word){
-	if(!this.words[word.length]){
-		return false;
-	}
-	const le = word.length;
-	if(!word.include('.')){
-		return this.words[len].include(word);
-	}
-	const reg = new RegExp(word);
-	return this.words[len].some((item)=>{
-		return reg.test(item);
-	});
+wordDictionary.prototype.search = function(word) {
+    if (!this.words[word.length]) {
+        return false;
+    }
+    const le = word.length;
+    if (!word.include('.')) {
+        return this.words[len].include(word);
+    }
+    const reg = new RegExp(word);
+    return this.words[len].some((item) => {
+        return reg.test(item);
+    });
 }
 
 // ********************链表的使用*****************
@@ -219,40 +224,40 @@ wordDictionary.prototype.search = function(word){
 
 // 输入：1->2->4, 1->3->4 输出：1->1->2->3->4->4
 
-function ListNode(value){
-	this.value = value;
-	this.next = null;
+function ListNode(value) {
+    this.value = value;
+    this.next = null;
 }
 
-const mergeTwoLists = function(l1,l2){
-	let head = new ListNode();
-	let cur = head;
-	while(l1 && l2){
-		if(l1.value <= l2.value){
-			cur.next = l1;
-			l1 = l1.next;
-		}else{
-			cur.next = l2;
-			l2 = l2.next;
-		}
-		cur = cur.next;
-	}
-	cur.next = l1!==null?l1:l2;
-	return head.next;
+const mergeTwoLists = function(l1, l2) {
+    let head = new ListNode();
+    let cur = head;
+    while (l1 && l2) {
+        if (l1.value <= l2.value) {
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+    cur.next = l1 !== null ? l1 : l2;
+    return head.next;
 }
 
 // 真题描述：给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
 
-const deleteDuplicates = functions(head){
-	let cur = head;
-	while(cur != null && cur.next != null){
-		if(cur.value == cur.next.value){
-			cur.next= cur.next.next;
-		}else{
-			cur = cur.next;
-		}
-	}
-	return head;
+const deleteDuplicates = function(head) {
+    let cur = head;
+    while (cur != null && cur.next != null) {
+        if (cur.value == cur.next.value) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return head;
 }
 
 
@@ -261,29 +266,29 @@ const deleteDuplicates = functions(head){
 // 输入: 1->2->3->3->4->4->5
 // 输出: 1->2->5
 
-const deleteDuplicates2  = function(head){
-	if(!head || !head.next){
-		return head;
-	}
-	let dumy = new ListNode();
-	dumy.next = head.next;
-	let cur = dumy;
-	while(cur.next && cur.next.next){
-		if(cur.next.val === cur.next.next.val){
-			let val = cur.next.val;
-			// 反复地排查后面的元素是否存在多次重复该值的情况
-            while(cur.next && cur.next.val===val) {
+const deleteDuplicates2 = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    let dumy = new ListNode();
+    dumy.next = head.next;
+    let cur = dumy;
+    while (cur.next && cur.next.next) {
+        if (cur.next.val === cur.next.next.val) {
+            let val = cur.next.val;
+            // 反复地排查后面的元素是否存在多次重复该值的情况
+            while (cur.next && cur.next.val === val) {
                 // 若有，则删除
-                cur.next = cur.next.next 
+                cur.next = cur.next.next
             }
-		}else{
-			cur = cur.next;
-		}
-	}
-	return dumy.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return dumy.next;
 }
 
-/*********************快慢指针与多指针***************/ 
+/*********************快慢指针与多指针***************/
 
 
 // 真题描述：给定一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
@@ -293,23 +298,23 @@ const deleteDuplicates2  = function(head){
 // 给定一个链表: 1->2->3->4->5, 和 n = 2.
 // 当删除了倒数第二个结点后，链表变为 1->2->3->5.
 
-const removeNthFromEnd = function(head,n){
-	let dumy = new ListNode();
-	dumy.next = head;
+const removeNthFromEnd = function(head, n) {
+    let dumy = new ListNode();
+    dumy.next = head;
 
-	let fast = dumy;
-	let slow = dumy;
+    let fast = dumy;
+    let slow = dumy;
 
-	while(n !=0 ){
-		fast = fast.next;
-		n--;
-	}
-	while(fast.next != null){
-		fast = fast.next ;
-		slow = slow.next;
-	}
-	slow.next = slow.next.next;
-	return dumy.next;
+    while (n != 0) {
+        fast = fast.next;
+        n--;
+    }
+    while (fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return dumy.next;
 }
 
 
@@ -321,17 +326,17 @@ const removeNthFromEnd = function(head,n){
 // 输入: 1->2->3->4->5->NULL
 // 输出: 5->4->3->2->1->NULL
 
-const reverseList = funciton(head){
-	let pre = null;
-	let cur = head;
-	while(cur != null){
-		let next = cur.next;
-		cur.next = pre;
+const reverseList = function(head) {
+    let pre = null;
+    let cur = head;
+    while (cur != null) {
+        let next = cur.next;
+        cur.next = pre;
 
-		pre = cur;
-		cur  = next;
-	}
-	return pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
 }
 
 // 局部反转一个链表
@@ -346,26 +351,350 @@ const reverseList = funciton(head){
 // 输入: 1->2->3->4->5->NULL, m = 2, n = 4
 // 输出: 1->4->3->2->5->NULL
 
-const reverseBetween  = function(head,m,n){
-	let pre,cur,leftHead;
-	const dumy = new ListNode();
-	dumy.next = head;
-	let p = dumy;
-	for(let i=0;i<m-1;i++){
-		p = p.next;
-	}
-	leftHead = p;
-	let start = leftHead.next;
-	pre = start;
-	cur = pre.next;
+const reverseBetween = function(head, m, n) {
+    let pre, cur, leftHead;
+    const dumy = new ListNode();
+    dumy.next = head;
+    let p = dumy;
+    for (let i = 0; i < m - 1; i++) {
+        p = p.next;
+    }
+    leftHead = p;
+    let start = leftHead.next;
+    pre = start;
+    cur = pre.next;
 
-	for (let i = m;i<n;i++){
-		let next = cur.next;
-		cur.next = pre;
-		pre = cur;
-		cur = next;
-	}
-	leftHead.next = prev;
-	start.next = cur;
-	return dummy.next;
+    for (let i = m; i < n; i++) {
+        let next = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = next;
+    }
+    leftHead.next = prev;
+    start.next = cur;
+    return dummy.next;
+}
+
+const findlowestString = (str) => {
+    var _str = str;
+    _str = _str.split('');
+    let _obj = {};
+    _str.forEach((item) => {
+        if (_obj[item]) {
+            _obj[item]++;
+        } else {
+            _obj[item] = 1;
+        }
+    });
+    console.log(_obj);
+    console.log(Object.values(_obj));
+    let _min = Math.min(...Object.values(_obj));
+    console.log(_min);
+}
+
+findlowestString('aabbc')
+
+// 真题描述：给定一个链表，判断链表中是否有环。
+
+// 示例 1：
+
+// 输入：[3,2,0,4]（链表结构如下图） 输出：true
+// 解释：链表中存在一个环
+
+// 入参是头结点 
+
+const hasCycle = function(head) {
+    while (head) {
+        if (head.flag) {
+            return true;
+        } else {
+            head.flag = true;
+            head = head.next;
+        }
+    }
+    return false;
+}
+
+// 环形链表衍生问题——定位环的起点
+
+// 真题描述：给定一个链表，返回链表开始入环的第一个结点。 如果链表无环，则返回 null。
+
+const detectCycle = function(head) {
+    while (head) {
+        if (head.flag) {
+            return head;
+        } else {
+            head.flag = true;
+            head = head.next;
+        }
+    }
+    return null;
+};
+
+
+/******************栈和队列的使用****************************/
+
+
+// 题目描述：给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+// 有效字符串需满足：
+
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+// 注意空字符串可被认为是有效字符串。
+const leftToRight = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+};
+
+const isValid = function(s) {
+    if (!s) {
+        return true;
+    }
+
+    const stack = [];
+    const len = s.length;
+    for (let i = 0; i < len; i++) {
+        let ch = s[i];
+        if (ch === "(" || ch === "[" || ch === "{") {
+            stack.push(leftToRight[ch]);
+        } else {
+            if (!stack.length || stack.pop() != ch) {
+                return false;
+            }
+        }
+    }
+    return !stack.length;
+}
+
+
+// 题目描述: 根据每日气温列表，请重新生成一个列表，对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+
+// 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+const dailyTemperatures = function(T) {
+    const len = t.length;
+    const stack = [];
+    const res = (new Array(len)).fill(0);
+    for (let i = 0; i < len; i++) {
+        while (stack.length && T[i] > T[stack[stack.length - 1]]) {
+            let top = stack.pop();
+            res[i] = i - top;
+        }
+        stack.push(i);
+    }
+    return res;
+}
+
+// 题目描述：设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。(push,pop,top,getMin)
+const minStick = function() {
+    this.stack = [];
+}
+minStick.prototype.pop = function() {
+    return this.stack.pop();
+}
+
+minStick.prototype.push = function(value) {
+    this.stack.push(value);
+}
+minStick.prototype.top = function() {
+    if (!this.stack || !this.stack.length) {
+        return;
+    };
+    return this.stack[this.stack.length - 1];
+}
+// 此方法的时间复杂度为 n
+minStick.prototype.getMin = function() {
+    let minValue = Infinity;
+    let len = this.stack.length;
+    for (let i = 0; i < len; i++) {
+        if (minValue > this.stack[i]) {
+            minValue = this.stack[i];
+        }
+    }
+    return minValue;
+}
+
+
+/*******/
+// 如何用栈实现一个队列？
+// 题目描述：使用栈实现队列的下列操作：
+
+// push(x) -- 将一个元素放入队列的尾部。
+// pop() -- 从队列首部移除元素。
+// peek() -- 返回队列首部的元素。
+// empty() -- 返回队列是否为空。
+
+const MyQueue = function() {
+    this.stack1 = [];
+    this.stack2 = [];
+}
+
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x);
+}
+MyQueue.prototype.pop = function() {
+    if (this.stack2.length <= 0) {
+        while (this.stack1.length !== 0) {
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+    return this.stack2.pop();
+}
+MyQueue.prototype.peek = function() {
+    if (this.stack2.length <= 0) {
+        while (this.stack1.length != 0) {
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+    const stack2Len = this.stack2.length;
+    return stack2Len && this.stack2[stack2Len - 1];
+};
+MyQueue.prototype.empty = function() {
+    return !this.stack1.length && !this.stack2.length;
+}
+
+// 题目描述：给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+
+// 示例:
+
+// 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3 输出: [3,3,5,5,6,7]
+
+// 1 双指针+遍历法
+let maxSlidingWindow = function(nums, k) {
+    const len = nums.length;
+    const res = [];
+    let left = 0;
+    let right = k - 1;
+    while (right < len) {
+        let max = callMax(nums, left, right);
+        res.push(max);
+        left++;
+        right++;
+    }
+    return res;
+}
+// 这个函数用来计算最大值
+function calMax(arr, left, right) {
+    if (!arr || !arr.length) {
+        return;
+    }
+    let maxNum = arr[left];
+    for (let i = left; i <= right; i++) {
+        if (arr[i] > maxNum) {
+            maxNum = arr[i];
+        }
+    }
+    return maxNum;
+}
+
+// 双端队列法
+let maxSlidingWindow = function(nums, k) {
+    const len = nums.length;
+    const res = [];
+    const deque = [];
+    for (let i = 0; i < len; i++) {
+        while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+        deque.push(i);
+        while (deque.length && deque[0] <= i - k) {
+            deque.shift();
+        }
+        if (i >= k - 1) {
+            res.push(nums[deque[0]]);
+        }
+    }
+    return res;
+}
+
+/*************深度优先搜索 DFS和BFS**********************/
+
+
+// 题目描述：给定一个没有重复数字的序列，返回其所有可能的全排列
+// 示例：   
+// 输入: [1,2,3]
+// 输出: [
+// [1,2,3],
+// [1,3,2],
+// [2,1,3],
+// [2,3,1],
+// [3,1,2],
+// [3,2,1]
+// ]
+let permute = function(nums) {
+    const len = nums.length;
+    const curr = [];
+    const res = [];
+    const visited = {};
+
+    function dfs(nth) {
+        if (nth == len) {
+            res.push(curr.slice());
+            return;
+        }
+        for (let i = 0; i < len; i++) {
+            if (!visited[nums[i]]) {
+                visited[nums[i]] = 1;
+                curr.push(nums[i]);
+                dfs(nth + 1);
+                curr.pop();
+                visited[nums[i]] = 0
+            }
+        }
+    }
+    dfs(0);
+    return res;
+}
+
+function BFS(root) {
+    const queue = [];
+    queue.push(root);
+    while (queue.length) {
+        const top = queue[0];
+        console.log(top.val);
+        if (top.left) {
+            queue.push(top.left);
+        }
+        if (top.right) {
+            queue.push(top.right);
+        }
+        queue.shift();
+    }
+}
+// 题目描述：给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+// 说明：解集不能包含重复的子集。
+const subsets = function(nums){
+    const res = [];
+    const len = nums.length;
+    const  subset = [];
+    dfs(0);
+    function dfs(index) {
+        res.push(subset.slice());
+        for(let i = index;i<len;i++){
+            subset.push(nums[index]);
+            dfs(i+1);
+            subset.pop();
+        }
+    }
+    return res;
+}
+
+// 题目描述：给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+const combine = function(n,k){
+    const res = [];
+    const subset = [];
+    dfs(1);
+    function dfs(index){
+        if(subset.length == k){
+            res.push(subset.slice());
+            return;
+        }
+        for(let i = index;i<n;i++){
+            subset.push(i);
+            dfs(i+1);
+            subset.pop();
+        }
+    }
+    return res;
 }
