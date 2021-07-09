8951dffe9362b9c492af0e094576a50dd29a6805
Nicosen <SUCCESSW2M@163.com>
2021-07-09 10:43:36 +0800

    快速排序

--- 前端算法和数据结构面试.js
+++ 前端算法和数据结构面试.js
@@ -1009,7 +1009,6 @@ const selectSort = function(arr){
     return arr;
 }
 // 插入排序
-
 const insertSort = function(arr){
     const len = arr.length;
     let temp;
@@ -1024,3 +1023,90 @@ const insertSort = function(arr){
     }
     return arr;
 }
+
+// 归并排序 归并排序的时间复杂度是 O(nlog(n))
+const mergeSort = function(arr){
+    const len = arr.length;
+    if(len<=1){
+        return arr;
+    }
+    const mid = Math.floor(len/2);
+    const leftArr = mergeSort(arr.slice(0,mid));
+    const rightArr = mergeSort(arr.slice(mid,len));
+    arr = mergeArr(leftArr,rightArr);
+    return arr;
+}
+const mergeArr = function(arr1,arr2){
+    let i =0,j=0;
+    const res = [];
+    const len1 = arr1.length;
+    const len2 = arr2.length;
+    while(i<len1 && j<len2){
+        if(arr1[i] < arr2[j]){
+            res.push(arr1[i]);
+            i++;
+        }else{
+            res.push(arr2[j]);
+            j++;
+        }
+    }
+    if(i<len1){
+        return res.concat(arr1.slice(i))
+    }else{
+        return res.concat(arr2.slice(j))
+    }
+}
+
+// 快速排序入口
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    // 定义递归边界，若数组只有一个元素，则没有排序必要
+    if(arr.length > 1) {
+        // lineIndex表示下一次划分左右子数组的索引位
+        const lineIndex = partition(arr, left, right)
+        // 如果左边子数组的长度不小于1，则递归快排这个子数组
+        if(left < lineIndex-1) {
+          // 左子数组以 lineIndex-1 为右边界
+          quickSort(arr, left, lineIndex-1)
+        }
+        // 如果右边子数组的长度不小于1，则递归快排这个子数组
+        if(lineIndex<right) {
+          // 右子数组以 lineIndex 为左边界
+          quickSort(arr, lineIndex, right)
+        }
+    }
+    return arr
+  }
+  // 以基准值为轴心，划分左右子数组的过程
+  function partition(arr, left, right) {
+    // 基准值默认取中间位置的元素
+    let pivotValue = arr[Math.floor(left + (right-left)/2)]
+    // 初始化左右指针
+    let i = left
+    let j = right
+    // 当左右指针不越界时，循环执行以下逻辑
+    while(i<=j) {
+        // 左指针所指元素若小于基准值，则右移左指针
+        while(arr[i] < pivotValue) {
+            i++
+        }
+        // 右指针所指元素大于基准值，则左移右指针
+        while(arr[j] > pivotValue) {
+            j--
+        }
+  
+        // 若i<=j，则意味着基准值左边存在较大元素或右边存在较小元素，交换两个元素确保左右两侧有序
+        if(i<=j) {
+            swap(arr, i, j)
+            i++
+            j--
+        }
+  
+    }
+    // 返回左指针索引作为下一次划分左右子数组的依据
+    return i
+  }
+  
+  // 快速排序中使用 swap 的地方比较多，我们提取成一个独立的函数
+  function swap(arr, i, j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]]
+  }
