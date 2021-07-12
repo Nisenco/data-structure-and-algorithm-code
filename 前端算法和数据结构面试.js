36da6bf4860b4dfede6b42c6a2319808dd22281e
Nicosen <SUCCESSW2M@163.com>
2021-07-12 16:33:18 +0800

    用例

--- 前端算法和数据结构面试.js
+++ 前端算法和数据结构面试.js
@@ -1274,3 +1274,131 @@ const copyRandomList = (head) => {
     // 注意这里返回的是copyHead而不是head
     return copyHead
 };
+
+// 题目描述：给出一个区间的集合，请合并所有重叠的区间。
+// 输入: [[1,3],[2,6],[8,10],[15,18]]
+// 输出: [[1,6],[8,10],[15,18]]
+// 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6]
+const merge = function(intervals){
+    const res = [];
+    const len = intervals.length;
+    intervals.sort((a,b)=>a[0]-b[0]);
+    if(!intervals || !intervals.length) {
+        return []
+    }
+    res.push(intervals[0]);
+    for(let i =1;i<len;i++){
+        prev = res[res.length-1];
+        if(prev[1] >= intervals[i][0]) {
+            prev[1] = Math.max(prev[1], intervals[i][1]);
+        } else {
+            res.push(intervals[i])
+        }
+    }
+    return res;
+}
+
+// 题目描述： 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+const lowestCommonAncestor  = function(root,p,q){
+    function dfs(root){
+        if(!root|| root===p|| root===q){
+            return root;
+        }
+        const leftNode = dfs(root.left);
+        const rightNode = dfs(root.right);
+        if(leftNode && rightNode){
+            return root;
+        }
+        return leftNode || rightNode;
+    }
+    dfs(root);
+}
+
+// 题目描述：给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+
+// 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+// 你可以假设 nums1 和 nums2 不会同时为空。
+
+// 示例 1:
+
+// nums1 = [1, 3]
+// nums2 = [2]
+// 则中位数是 2.0
+
+const findMedianSortedArrays = function(num1,num2){
+    const len1 = num1.lenght;
+    const len2 = num2.length;
+    if(len1>len2){
+        return findMedianSortedArrays(num2,num1);
+    }
+    // 计算两个数组的总长度
+    const len = len1 + len2 
+    // 初始化第一个数组“下刀”的位置
+    let slice1 = 0  
+    // 初始化第二个数组“下刀”的位置
+    let slice2 = 0  
+    // 初始化第一个数组二分范围的左端点
+    let slice1L = 0  
+    // 初始化第一个数组二分范围的右端点
+    let slice1R = len1  
+    let L1, L2, R1, R2
+    // 当slice1没有越界时
+    while(slice1 <= len1) {
+        // 以二分原则更新slice1
+        slice1 = Math.floor((slice1R - slice1L)/2) + slice1L  
+        // 用总长度的1/2减去slice1，确定slice2
+        slice2 = Math.floor(len/2) - slice1 // 计算L1、L2、R1、R2
+        const L1 = (slice1===0)? -Infinity : nums1[slice1-1]
+        const L2 = (slice2===0)? -Infinity : nums2[slice2-1]  
+        const R1 = (slice1===len1)? Infinity : nums1[slice1]  
+        const R2 = (slice2===len2)? Infinity: nums2[slice2]
+        
+        // 处理L1>R2的错误情况
+        if(L1 > R2) {
+            // 将slice1R左移，进而使slice1对应的值变小
+            slice1R = slice1 - 1
+        } else if(L2 > R1) {
+            // 反之将slice1L右移，进而使slice1对应的值变大
+            slice1L = slice1 + 1
+        } else {
+            // 如果已经符合取中位数的条件（L1<R2&&L2<R1)，则直接取中位数
+            if(len % 2 === 0) {
+                const L = L1 > L2 ? L1 : L2  
+                const R = R1 < R2 ? R1 : R2
+                return  (L + R)/2
+            } else {
+                const median = (R1 < R2) ? R1 : R2  
+                return median
+            }
+        }
+
+    }
+    return -1
+};
+// 题目描述: 假如有一排房子，共 n 个，每个房子可以被粉刷成红色、蓝色或者绿色这三种颜色中的一种，你需要粉刷所有的房子并且使其相邻的两个房子颜色不能相同。
+// 输入: [[17,2,17],[16,16,5],[14,3,19]]
+// 输出: 10
+// 解释: 将 0 号房子粉刷成蓝色，1 号房子粉刷成绿色，2 号房子粉刷成蓝色。
+// 最少花费: 2 + 5 + 3 = 10。
+
+// 命题关键字：动态规划、滚动数组
+const minCost = function(costs) {
+    // 处理边界情况
+    if(!costs || !costs.length) return 0 
+    // 缓存房子的个数
+    const len = costs.length
+    // 开始更新状态
+    for(let i=1;i<len;i++) {  
+        // now表示粉刷到当前房子时对应的价格状态
+        const now = costs[i]  
+        // prev表示粉刷到上一个房子时的价格状态
+        const prev = costs[i-1]  
+        // 更新当前状态下，刷三种油漆对应的三种最优价格
+        now[0] += Math.min(prev[1], prev[2])  
+        now[1] += Math.min(prev[0], prev[2])  
+        now[2] += Math.min(prev[1], prev[0])
+    }
+    // 返回粉刷到最后一个房子时，总价格的最小值
+    return Math.min(costs[len-1][0], costs[len-1][1], costs[len-1][2])
+};
